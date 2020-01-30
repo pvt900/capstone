5e0141770d2193fb80101ee1177f5e898a4f9562
Robert <robert.farmer@wartburg.edu>
2020-01-30 16:26:45 -0600

    Added Notes from Zelle Meeting

--- MechSoup.py
+++ MechSoup.py
@@ -11,4 +11,19 @@ table = browser.get_current_page().find('table')
 print(type(table))
 rows = table.get_text().split('\n') # List of all Class Rows split by \n. 
 
+
 print(type(rows))
+# 
+# Methods for Error-Checking Submission
+# return list of options and set by index?
+# Return NExt Week with Concrete API Proposal for Live & Historic
+    # Ensure all Queries are Valid.
+# Work w/ Setting Form Fields via Script 
+# 2 Very Different Pieces (Historic Data & "Hot" Queries)
+# Flag that when object is created. Do you want to update Records?
+# Function to check Terms in the Historic Data
+    # Save Old Data Somehow?? - Backup File. Backup Sheet?
+    # CSV's rather than using Openpyxl look at Pandas
+    # Don't Foresee using Excel, Presenting Info using Excel/OpenOffice
+    # Is there a Panda's file for multiple versions of a dataset
+    # Take a look if there is something Panda-y we can use to store data present next time.
