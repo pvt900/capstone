379e6b70ae3cea3a664ef1681c1c6c0a3459986d
Robert <robert.farmer@wartburg.edu>
2020-02-10 17:51:38 -0600

    cf_api v4, TechTalkFIle updates

--- TechTalkFile.py
+++ TechTalkFile.py
@@ -3,17 +3,15 @@ from cf_api import search as cfapi
 
 def main():
     test = cfapi()
-    #test.get_deptartments()
+    test.get_deptartments()
     test.by_department('Computer Science')
     test.by_instructor('65105')
     test.get_terms()
     test.by_term(2018,'Fall Term')
     test.search_form()
-    #test.display_browser()
-    #test.save_file()
+    test.display_browser()
+    test.save_file()
     
     
-
-
 if __name__ == '__main__':
     main()
--- cf_api.py
+++ cf_api.py
@@ -3,19 +3,17 @@
 import mechanicalsoup
 from bs4 import BeautifulSoup as soup
 
-#values = self.browser.get_current_page().findAll('option')
-#for item in values:
-#    valuetext = str(item).split('"')
-#    option = valuetext[1] + ' ' + item.get_text()
-#    print(option)
-
 class search:    #uses helper functions to query and pull results
 
     def __init__(self):
+        '''
+        Initializes Variables for use within the Search class
+        '''
         #Sets Statefulself.browser Object to winnet then it it grabs form
         self.browser = mechanicalsoup.StatefulBrowser()
         self.browser.open("http://winnet.wartburg.edu/coursefinder/")
         self.page = self.browser.select_form()
+        
         #Form Variables
         self.keyword = None
         self.dept = None
@@ -28,6 +26,7 @@ class search:    #uses helper functions to query and pull results
         self.pass_fail = False
         self.instructor = None
         self.course_open = None
+        
         #DropDownDictionaries
         self.Departments = {'All':'None Selected','AC   ':'Accounting', 'ART  ':'Art','BI   ':'Biology','BA   ':'Business Administration','CH   ':'Chemistry', 'CS   ':'Computer Science',
                'CCJ  ':'Criminology/Criminal Justice','EC   ':'Economics', 'ED   ':'Education', 'ES   ':'Engineering Science', 'EN   ':'English','EI   ':'English International Students',
@@ -36,55 +35,59 @@ class search:    #uses helper functions to query and pull results
                'ID   ':'Interdisciplinary','COM  ':'Journalism and Communication','LS   ':'Liberal Studies','MA   ':'Mathematics', 'MU   ':'Music',
                'NSC  ':'Neuroscience','PJ   ':'Peace and Justice Studies','PH   ':'Philiosophy','PE   ':'Physical Education','PHY  ':'Physics','PS   ':'Political Science',
                'NUR  ':'Pre-Professional Nursing Prog', 'PSY  ':'Psychology', 'PBH  ':'Public Health','RE   ':'Religion','SCH  ':'Scholars Program',
-               'SW   ':'Social Work','SO   ':'Sociology','SP   ':'Spanish','TH   ':'Theatre','VE   ':'Venture Ed','WW   ':'Wartburg West','WS   ':"Womens Studies"} # Automate to add rest to Dictionary
-
-        self.Terms = ['May Term', 'Summer Session', 'Winter Term', 'Fall Term'] #Maybe Automate to Provide Year Options Instead of All?
-
+               'SW   ':'Social Work','SO   ':'Sociology','SP   ':'Spanish','TH   ':'Theatre','VE   ':'Venture Ed','WW   ':'Wartburg West','WS   ':"Womens Studies"} 
+        self.Terms = ['May Term', 'Summer Session', 'Winter Term', 'Fall Term'] 
         self.Times = {'all':'Show all', 'od':'Other Evening', 'oe':'Other Evening', '7:45AM/MWF':'7:45AM MWF', '9:00AM/MWF':'9:00AM MWF', '10:45AM/MWF':'10:45AM MWF',
-                        '12:00PM/MWF':'12:00PM MWF', '1:15PM/MWF':'1:15PM MWF', '2:30PM/MWF':'2:30PM MWF', '3:50PM/MWF':'3:50PM MWF', '7:45AM/THX':'7:45AM TH',
-                        '7:45AM/THX':'7:45AM TH', '9:35AM/THX':'9:35AM TH','11:30AM/THX':'11:30AM TH', '1:00PM/THX':'1:00PM TH', '2:50PM/THX':'2:50PM TH', '3:50PM/THX':'3:50PM TH'}
-
+                    '12:00PM/MWF':'12:00PM MWF', '1:15PM/MWF':'1:15PM MWF', '2:30PM/MWF':'2:30PM MWF', '3:50PM/MWF':'3:50PM MWF', '7:45AM/THX':'7:45AM TH',
+                    '7:45AM/THX':'7:45AM TH', '9:35AM/THX':'9:35AM TH','11:30AM/THX':'11:30AM TH', '1:00PM/THX':'1:00PM TH', '2:50PM/THX':'2:50PM TH', '3:50PM/THX':'3:50PM TH'}
         self.ED = {'all':'Show all EE courses', 'CP':'CP - Capstone', 'FL':'FL- Foreign Language', 'FR':'FR - Faith & Reflection',
                     'HF':'HF - Humanities/Fine Arts Interconnected','MR':'MR - Mathematical Reasoning','NS':'NS - Natural Science Interconnected',
                     'SR':'SR - Scientific Reasoning','SS':'SS - Social Science Interconnected'}
-
-        self.CD = {'none':'Not selected','C':'C - Cultural Immersion', 'D': 'D - Diversity Across Curriculum Course'}
+        self.CD = {'none':'Not selected','C':'C - Cultural Immersion', 'D': 'D - Diversity Across Curriculum Course','all':'Show both'}
         self.WI = {'none':'Not selected', 'WI':'Writing Intensive'}
         self.PF = {'none':'Not selected', 'PF':'Pass/D/F basis'}
         self.Instructor = {'0':'Not Selected'}
 
        
     def by_keyword(self,key):
-        #inputs user keyword into course finder
-        keyword = key
-        self.page.set('ctl00$ContentPlaceHolder1$FormView1$TextBox_keyword', keyword)
+        '''
+        Takes parameter to be passed into the Course Finder Keyword Search
+        '''
+        self.page.set('ctl00$ContentPlaceHolder1$FormView1$TextBox_keyword', key)
 
     def get_deptartments(self):
-        #pulls current list of departments
-        #and their corresponding codes from winnet
-        #select.form(selector = 'ctl00$ContentPlaceHolder1$FormView1$DropDownList_Department')
+        '''
+        Pulls current lists of departments for Department drop down
+        '''
         print('Possible Departments')
         print('_____________________')
         for key,value in self.Departments.items():
             print(value)
             
     def by_department(self,department):
-        #selects department for course finder
+        '''
+        Takes department as a parameter and passes corresponding department code to form
+        '''
         dept = [k for k,v in self.Departments.items() if v.casefold() == department.casefold()]
         self.page.set("ctl00$ContentPlaceHolder1$FormView1$DropDownList_Department", dept[0])
 
     def get_terms(self):    
-        #pulls list of current available terms to query
-        #pull each option, append to list
+        '''
+        Pulls list of current terms, terms are case-sensitive
+        '''
         print('Term Terminology')
         print('_____________________')
         for term in self.Terms:
             print(term)
 
     def by_term(self,year,semester):
-        #selects term from course finder
+        '''
+        Takes year and semester as parameters
+        year is the school year
+        semester is the calendar semester
+        passes parameters to form as one string
+        '''
         term = str(year)+' '+semester
-        print(term)
         self.page.set("ctl00$ContentPlaceHolder1$FormView1$DropDownList_Term", term)
 
     #def get_times():
@@ -92,43 +95,58 @@ class search:    #uses helper functions to query and pull results
         #pull each option, append to list
         
     def by_time(self,time):
-        #selects class meeting time in course finder
+        '''
+        Takes time as parameter and passes the proper form value to search form
+        '''
         self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_MeetingTime', time)
 
-    #def get_ee_reqs():
+    #def get_ee_reqs(self):
         #pulls list of essential es class types
         #pull each option, append to list
 
-    def by_ee_req(self,ee):
-        #selects essential ed class type
-        if ee == 'Y':
-            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_EssentialEd', 'all')
-        elif ee == 'N':
-            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_EssentialEd', 'none')
-        else:
-            print("Invalid input, function only accepts 'Y' or 'N'")
-        
-   # def get_cult_div_reqs():
-        #pulls list of 
-        #pull each option, append to list
+    def by_ee(self,ee):
+        '''
+        Takes ee as essential ed variable and passes to form        
+        '''
+        self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_EssentialEd', ee)
 
-    def is_cult_div(self,option):
-        if option == 'C':
-            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_CulturalDiversity', 'C')
-        elif option == 'D':
-            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_CulturalDiversity', 'D')
-        elif option == "BOTH":
-            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_CulturalDiversity', 'all')
-        else:
-            print("Please enter C, D, or BOTH. If you do not want to select an option, do not use this function.")
+   def get_culdiv(self):
+        '''
+        Retrieves Cultural Diversity Variables for form search
+        '''
+    
+        print('Cultural Diversity Options')
+        print('_____________________')
+        for key,value in self.CD.items():
+            print(value)
+            
+    def is_culdiv(self,option):
+        '''
+        Takes option as a parameter and passes it to form
+        option is the case-sensitive variables from get_culdiv()
+        '''
+        cdo = [k for k,v in self.CD.items() if v.casefold() == option.casefold()]
+        self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_CulturalDiversity', cdo[0])
 
-    def is_writing(self):
-        #selects Writing Intensive option
-        self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_WritingIntensive', 'WI')
+    def is_writing(self, wi):
+        '''
+        Takes wi as a parameter and passes it to form
+        wi is either True or False
+        '''
+        if wi == True:
+            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_WritingIntensive', 'WI')
+        elif wi == False:
+            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_WritingIntensive', 'none')
 
-    def is_pass_fail(self):
-        #selects Pass/Fail classes
-        self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_PassFail', 'PF')
+    def is_pass_fail(self,pf):
+        '''
+        Takes pf as parameter passes to form
+        pf is either TRUE or FALSE
+        '''
+        if pf == True:
+            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_PassFail', 'PF')
+        elif pf == False:
+            self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_PassFail','none')
 
     #def get_instructors():
         #gets list of all current instructors
@@ -138,22 +156,31 @@ class search:    #uses helper functions to query and pull results
         #selects instructor in dropdown menue
         self.page.set('ctl00$ContentPlaceHolder1$FormView1$DropDownList_Instructor', id_number)
 
-    def couse_open(self):
-        #selects checkbox
-        self.page.set('ctl00$ContentPlaceHolder1$FormView1$CheckBox_OpenCourses', True)
+    def couse_open(self, option):
+       '''
+       Takes option as a parameter
+       parameter is True or False and passes that to a checkbock in form
+       '''
+        self.page.set('ctl00$ContentPlaceHolder1$FormView1$CheckBox_OpenCourses', option)
     def search_form(self):
-        #Submits Course Search
+        '''
+        selects and submits the form's search button
+        '''
         self.page.choose_submit('ctl00$ContentPlaceHolder1$FormView1$Button_FindNow')
-        self.browser.submit_selected()# Submits Form. Retrieves Results.
+        self.browser.submit_selected()
         
     def display_browser(self):
-        #Displays self.browser Windows of Current Page
+        '''
+        Displays cached copy of webpage
+        '''
         self.browser.launch_browser()
 
     def save_file(self):
+        '''
+        Saves the Results to a .csv file
+        '''
         table_of_data = self.browser.get_current_page().find('table')
         data = table_of_data.get_text().split('\n')
-
         with open('test_data.txt', 'w') as handler: #output of scraped data
             print("writing file...")
             for listitem in data:
